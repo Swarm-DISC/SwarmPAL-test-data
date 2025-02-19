67eee4e94c8d1542e0969e34f2496e8afc10971e
David de Klerk <david.deklerk@ed.ac.uk>
2025-02-19 15:52:52 +0000

    Move registry to data/

--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,7 @@
 import argparse
 import yaml
 from pathlib import Path
+import hashlib
 
 from datetime import timedelta
 
@@ -11,16 +12,20 @@ def str_to_timedelta(time):
     hours, minutes, seconds = (int(part) for part in time.split(":"))
     return timedelta(hours=hours, minutes=minutes, seconds=seconds)
 
+def calc_md5sum(filename):
+    '''Calculate the md5sum of the content of a file'''
+    return hashlib.md5(open(filename, 'rb').read()).hexdigest()
+
 def main(args):
 
     with open(args.data_params) as f:
         datasets = yaml.safe_load(f)
     
+    registry = {}
     for name, dataset in datasets.items():
         provider = dataset.get('provider', 'none')
         print(f"Downloading '{name}' from {provider}")
 
-        print(name, dataset)
         # Convert pad_times from strings to timedelta objects
         if 'pad_times' in dataset['params']:
             dataset['params']['pad_times'] = [
@@ -39,7 +44,14 @@ def main(args):
             continue
 
         filename = f'{name}.nc4'
-        data.to_netcdf(args.data_dir / filename)
+        filepath = args.data_dir / filename
+
+        data.to_netcdf(filepath)
+        registry[filename] = calc_md5sum(filepath)
+
+    with open(args.data_dir / args.registry, 'w') as f:
+        for filename, md5sum in registry.items():
+            f.write(f'{filename} md5:{md5sum}\n')
 
 
 if __name__ == '__main__':
@@ -49,6 +61,9 @@ if __name__ == '__main__':
     )
     parser.add_argument('-d', '--data-dir', default='data', type=Path, 
         help='Directory where files will be saved to.'
+    )
+    parser.add_argument('-r', '--registry', default='registry.txt',
+        help='Filename for the pooch registry file'
     )
     parser.add_argument('data_params', 
         help='A filename for YAML formatted descriptions of each data set to generate.'
